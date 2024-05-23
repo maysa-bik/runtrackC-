abd340c8b67f9d08f737d2a9d7e89c1d0808a5b0
maysa-bik <maysa.bik@laplateforme.com>
2024-05-23 12:23:22 +0200

    commencer le jour5

---
+++ jour05/Job05/Etudiant.hpp
@@ -0,0 +1,25 @@
+#ifndef ETUDIANT_HPP
+#define ETUDIANT_HPP
+
+#include <string>
+
+class Etudiant {
+private:
+    std::string nom;
+    std::string prenom;
+    int age;
+    int matricule;
+
+public:
+    // Constructeur avec une liste d'initialisation
+    Etudiant(const std::string& nom, const std::string& prenom, int age, int matricule)
+        : nom(nom), prenom(prenom), age(age), matricule(matricule) {}
+
+    // Méthodes pour accéder aux propriétés de l'étudiant
+    const std::string& getNom() const { return nom; }
+    const std::string& getPrenom() const { return prenom; }
+    int getAge() const { return age; }
+    int getMatricule() const { return matricule; }
+};
+
+#endif // ETUDIANT_HPP
